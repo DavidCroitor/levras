ccd2b5bf15f4dcebc8bd2ebf9b18faf51e6449ec
DavidCroitor <davidcroitor.dc@gmail.com>
2026-08-23 23:00:47 +0300

    Initial commit

---
+++ src/levras.Presentation/Views/MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using AvaloniaEdit.Document;
+
+namespace Presentation.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        
+    }
+}
